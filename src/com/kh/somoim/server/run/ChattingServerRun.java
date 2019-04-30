04f10806ebafae9464f6e1127763e5991afae4ce
ko <ko@DESKTOP-7MDR7BJ>
2019-04-30 15:11:51 +0900

    채텅 서버 추가

---
+++ src/com/kh/somoim/server/run/ChattingServerRun.java
@@ -0,0 +1,13 @@
+package com.kh.somoim.server.run;
+
+import com.kh.somoim.chattingServer.ChattingServer;
+
+public class ChattingServerRun {
+
+	public static void main(String[] args) {
+		
+		new ChattingServer().serverStart();
+		
+	}
+	
+}
